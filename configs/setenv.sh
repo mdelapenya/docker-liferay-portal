20efcf793c832b246a32feca0deb49435a726f3e
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-02-19 12:16:04 +0100

    (#15) Support debug mode
    If the DEBUG_MODE env var is present on runtime, then the Liferay Portal instance will be run on debug mode, using port 9000 for debug connections

--- configs/setenv.sh
+++ configs/setenv.sh
@@ -1,1 +1,7 @@
 CATALINA_OPTS="$CATALINA_OPTS -server -Dfile.encoding=UTF8 -Xms2048m -Xmx2048m -XX:MaxPermSize=512m -XX:NewSize=200m -XX:MaxNewSize=200m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=20 -XX:ParallelGCThreads=2 -Djava.net.preferIPv4Stack=true"
+
+if [ ! -z $DEBUG_MODE ] ; then
+    DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9000"
+
+    CATALINA_OPTS="${CATALINA_OPTS} ${DEBUG}"
+fi
