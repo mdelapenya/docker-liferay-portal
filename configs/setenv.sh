068d3c6a100fcd282cef5885c323b010923a522b
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-05-16 13:01:16 +0200

    Allow tuning JVM main memory

--- configs/setenv.sh
+++ configs/setenv.sh
@@ -1,4 +1,6 @@
-CATALINA_OPTS="$CATALINA_OPTS -server -Dfile.encoding=UTF8 -Xms2048m -Xmx2048m -XX:MaxPermSize=512m -XX:NewSize=200m -XX:MaxNewSize=200m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=20 -XX:ParallelGCThreads=2 -Djava.net.preferIPv4Stack=true"
+JVM_TUNING_MEMORY=${JVM_TUNING_MEMORY:-2048m}
+
+CATALINA_OPTS="$CATALINA_OPTS -server -Dfile.encoding=UTF8 -Xms${JVM_TUNING_MEMORY} -Xmx${JVM_TUNING_MEMORY} -XX:MaxPermSize=512m -XX:NewSize=200m -XX:MaxNewSize=200m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=20 -XX:ParallelGCThreads=2 -Djava.net.preferIPv4Stack=true"
 
 if [ ! -z $DEBUG_MODE ] ; then
     DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9000"
