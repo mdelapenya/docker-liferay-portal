a09ebea47246537e24b9a59b7961cb1703a54a19
Manuel de la Peña <mdelapenya@gmail.com>
2019-04-09 18:24:02 +0200

    (#24) Add LIFERAY_JVM_OPTS env var

--- configs/setenv.sh
+++ configs/setenv.sh
@@ -1,4 +1,5 @@
 CATALINA_OPTS="$CATALINA_OPTS -server -Dfile.encoding=UTF8 -Xms2048m -Xmx2048m -XX:MaxPermSize=512m -XX:NewSize=200m -XX:MaxNewSize=200m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=20 -XX:ParallelGCThreads=2 -Djava.net.preferIPv4Stack=true"
+CATALINA_OPTS="$CATALINA_OPTS ${LIFERAY_JVM_OPTS}"
 
 if [ ! -z $DEBUG_MODE ] ; then
     DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9000"
